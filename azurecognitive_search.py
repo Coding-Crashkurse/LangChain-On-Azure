68b4bec86e6381db3f66170a83ed8ce63890d6db
markus <datamastery87@gmail.com>
2023-08-12 15:53:42 +0200

    update code

--- azurecognitive_search.py
+++ azurecognitive_search.py
@@ -2,33 +2,33 @@ import openai
 import os
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.azuresearch import AzureSearch
+from langchain.document_loaders import AzureBlobStorageContainerLoader
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from dotenv import load_dotenv
 
+load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = "sk-"
-os.environ["OPENAI_API_VERSION"] = "2023-05-15"
 model: str = "text-embedding-ada-002"
 
+vector_store_address: str = f"https://{os.environ.get('AZURE_COGNITIVE_SEARCH_SERVICE_NAME')}.search.windows.net"
 
-vector_store_address: str = "YOUR_AZURE_SEARCH_ENDPOINT"
-vector_store_password: str = "YOUR_AZURE_SEARCH_ADMIN_KEY"
-
+print(vector_store_address)
 
 embeddings: OpenAIEmbeddings = OpenAIEmbeddings(deployment=model, chunk_size=1)
 index_name: str = "langchain-vector-demo"
 vector_store: AzureSearch = AzureSearch(
     azure_search_endpoint=vector_store_address,
-    azure_search_key=vector_store_password,
+    azure_search_key=os.environ.get("AZURE_COGNITIVE_SEARCH_API_KEY"),
     index_name=index_name,
     embedding_function=embeddings.embed_query,
 )
 
-from langchain.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
-
-loader = TextLoader("../../../state_of_the_union.txt", encoding="utf-8")
-
+loader = AzureBlobStorageContainerLoader(conn_str=os.environ.get("AZURE_CONN_STRING"), container=os.environ.get("CONTAINER_NAME"))
 documents = loader.load()
+
 text_splitter = CharacterTextSplitter(chunk_size=150, chunk_overlap=20)
 docs = text_splitter.split_documents(documents)
-
 vector_store.add_documents(documents=docs)
+
+print("Data loaded into vectorstore successfully")
